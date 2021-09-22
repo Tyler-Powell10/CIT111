fc81574222e90ab392448ff96e4c4040558a4287
Tyler-Powell10 <56084361+Tyler-Powell10@users.noreply.github.com>
2021-09-22 12:21:05 -0400

    Rand project

---
+++ Random/src/random/RandomExample.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package random;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomExample {
+
+    private final Random random;
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("you are so damn cute");
+        list.add("you are absolutley goergous");
+        list.add("you make my life complete");
+        list.add("you treat me incredible");
+        list.add("you are my soulmate");
+        list.add("you are my other half");
+        list.add("you are beautiful");
+        list.add("the small things get me like crazy");
+        list.add("I love your smile");
+        list.add("this list could go on endlessly");
+        list.add("I love your laugh");
+        list.add("I love the way you look at me");
+        list.add("I love your sense of humor and how 95% of it goes over my head");
+        list.add("I love that you are kinda a nerd ;) ");
+        list.add("you are my princess");
+        list.add("you is baby");
+        list.add("I can not wait to marry you :) ");
+        list.add("you make my dick hard");
+        list.add("that pussy is insane");
+        list.add("how wet i make you");
+        list.add("you call me daddy");
+        list.add("we have our whole future planned out down to every little detail ");
+        list.add("you make my heart stop");
+        list.add("my heart skips a beat everytime i see you");
+        list.add("I get butterflies when we kiss");
+        list.add("I have never doubted you");
+        list.add("you inspire me");
+        list.add("you mke me beyond happy");
+        list.add("I love the way you react to me");
+        list.add("I love the baby voice");
+        list.add("you understand me");
+        list.add("you care about my feelings deeply");
+        list.add("I like your butt :) ");
+        list.add("your body shape is sexy as fuck ");
+        list.add("your kisses are incredible");
+        list.add("i love the way your lips taste");
+        list.add("you have a pretty pussy");
+        list.add("you make me Horny");
+        list.add("The way you suck my dick");
+        list.add("I fell in love with your soul");
+        list.add("I fell in love with your mind");
+        list.add("I fell in love with your heart");
+        list.add("I trusted you right away, and you did not make me regret it");
+        list.add("you are my safe place");
+        list.add("you are my home");
+        list.add("the way we make love");
+        list.add("you are the person i want to come home to everyday");
+        list.add("you are gonna be a great mom");
+        list.add("we is gonna make cute babies");
+        list.add("we will be the best parents");
+        list.add("you take away all the hurt");
+        list.add("you are the first person i come to ");
+        list.add("I love the taste of your pussy");
+        list.add("I can never get enough of you");
+        list.add("I miss you even after i just saw you 5 minutes ago");
+        list.add("everything feels so right");
+        list.add("I think about our wedding day every night");
+        list.add("I picture our house together");
+        list.add("I picture our kids together");
+        list.add("All i can think about is falling asleep and waking up next to you one day");
+        list.add("you are sexy");
+        list.add("you are pretty");
+        list.add("you are beautiful");
+        list.add("youre incredible");
+        list.add("I have never met anyone with a heart as big as yours");
+        list.add("I love the way that you get *annoyed* with me when i is just lovin you");
+        list.add("That cute little eye roll thing you do");
+        list.add("your smile can change my whole day");
+        list.add("your hugs can fix even the biggest problems");
+        list.add("You are the first person i have cried to");
+        list.add("you the first person i have cried to");
+        list.add("you make my heart happy");
+        list.add("you make my soul happy");
+        list.add("I can be myself around you");
+        list.add("you know whats best for me even if i dont");
+        list.add("you always have my best interest");
+        list.add("you treat me like a princess even though you hate that name :) ");
+        list.add("I genuinley struggles not being able to use the semi-colon becuase i always wanna flirt with you");
+        list.add("you still flirt with me");
+        list.add("you genuinley think i am the greatest person in this world even when i am not");
+        list.add("you have faith in me ");
+        list.add("you trust me with all your heart");
+        list.add("you are patient and understanding");
+        list.add("I cant ever finish this list and give you this because there is so many reasons");
+        list.add("you are precious");
+        list.add("i look at you and amazed every day, i feel so damn lucky");
+        list.add("you make me vulnerable in the best way");
+        list.add("you have given me a life i never imagined i would have, you give me goals motivation to be the best for you");
+        list.add("you have a little voice in my head");
+        list.add("i feel such a strong connection with you it is insane, we just vibe and are genuinley meant to be together");
+        list.add("you made me believe in love again");
+        list.add("at the end of the day, no matter what, you make me smile, you dont go to bed if im not okay");
+        list.add("you are sentimental");
+        list.add("the little things make you the happiest");
+        list.add("you are a nerd and i love every bit of it");
+        list.add("you have pretty hair");
+        list.add("you look like a model without makeup and that shits impressive");
+        list.add("you have pretty teeth");
+        list.add("you take care of me");
+        list.add("you always check up on me ");
+        list.add("you are honest");
+        list.add("you are pure");
+        list.add("i look at you and get lost becuase i feel so blessed for you to be in my life");
+        list.add("you were my first happy cry");
+        list.add("i cant wait to see you walk down the isle and ball because you are gonna look so stunning i picture it everyday");
+        list.add("you are the most important priority in my life");
+        list.add("you have helped me get through so much stuff");
+        list.add("we got together at the perfect time, we will really know and understand our pasts, and eachother in the future");
+        list.add("you know every detail about me");
+        list.add("I fucking love you mia");
+        list.add("your names gonna be mia powell someday and that shit sounds perfect");
+        list.add("look down at your finger");
+        list.add("** i fucked this up scroll right** youre really lucky if you get this one princess.....i am layin here at 3am happy crying even after you told me to get sleep after a bad day lol..."
+                + "however...i cant stop thinking about how lucky i am...i have you...you might not see it but i have this amazing girl in my life who treats like a damn "
+                + "princess...i am absolutley fully 100 percent addicted to you, i can never get enough of you, not hugs, cuddles, kisses, talking, texting, any "
+                + "of it...you are really something special, you are jaw dropping... i cant beleive i am really mias FIANCE...HOW FUCKING BLESSED!!!!! your hugd oh my god"
+                + "they can fix anything, your kisses, well shit those have so much love and compassion its crazy, straight out of a movie. you are PERFECT.....and everythings"
+                + "so natural too like i know we are meant together, everything just flows so good, i dont have to put effort in to *putting effort in* it just comes natural, i love and "
+                + "care about you its just what i do, youre my priority, im gonna make sure you are okay, im gonna make sure you are happy, and im gonna make sure you are feeling"
+                + "loved forever and always....I do everything i can for you and i give you all that i have in me...i can always count on you no matter what...as soon as i wake uo"
+                + "you are my immediate though, i go to bed dreaming about our amazing future together, you are my world.... you melt my heart the way you support me and when"
+                + "im feeling pain you feel it too, it shows you really do care about me like no one else has or ever will, i can not wait until our kids get to see our happy family "
+                + "and see how much mommy and DADDY love eachother... we make an amazing team love, you bring out all of my positive traits i appreciate you so much....im so geeked i get to spend the rest of my life with you "
+                + "thank you for being exactly what i need. ***i wanted to put this in the middle ut i figured out it was kinda impossible to put this on your phone but just give me"
+                + "a few months and ill add 1000000000000 more reasons and ill make that shit like daily bread lmao, youll get a little random notification every morning about a reason"
+                + "i love you :) seeeeeee being a computer nerd isnt all that bad... i gonna use it to show you how much i love you... just like when we get our house"
+                + "im gonna put those contracting skills to work, need a walk in closet? got you. shit gonna have places for all your shoes and be nice and organized "
+                + "some led lights under each one so you can see, with a vanity and a big mirror that you can see front and back in.... i got you princess... a;ways and forever...a;sp i think "
+                + "whenever i add to this im gonna put in old paragraphs and cute messages i sent you in there thats saved lol.... i love you endlessly...i love you unconditionally***");
+
+        RandomExample obj = new RandomExample();
+        for (int i = 0; i < 1; i++) {
+
+            System.out.println(obj.getRandomList(list));
+
+        }
+
+    }
+
+    public RandomExample() {
+        this.random = new Random();
+    }
+
+    public String getRandomList(List<String> list) {
+
+        // create loop with user input
+        int love = random.nextInt(list.size());
+
+        System.out.println("reason i am in love with you number: " + love);
+
+        //list.remove(love);
+        return list.get(love);
+
+    }
+}
