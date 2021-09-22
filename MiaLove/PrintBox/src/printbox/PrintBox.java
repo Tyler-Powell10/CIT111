82fe8a5e7822271248556154aa4f05d1d400d9bd
Tyler-Powell10 <56084361+Tyler-Powell10@users.noreply.github.com>
2021-09-22 12:22:04 -0400

    Mia project

---
+++ MiaLove/PrintBox/src/printbox/PrintBox.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printbox;
+
+/**
+ *
+ * @author tylerpowell
+ */
+public class LoopPrinting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+            
+        
+        
+        
+        
+       
+        
+    } // end main
+    
+} // end class
---
+++ MiaLove/src/mialove/MiaLove.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mialove;
+import java.util.List;
+import java.util.ArraysList;
+import java.util.Random;
+/**
+ *
+ * @author tylerpowell
+ */
+public class MiaLove {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private Random random = new Random();
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        List<String> list = new ArrayList<String>();
+        list.add("baby");
+        list.add("love");
+        
+        RandomExample obj = new RandomExample();
+        for(int i = 0; i < 10; i++){
+            sout
+        }
+    }
+    
+}
